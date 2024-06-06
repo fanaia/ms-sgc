eda673f68b130db0543f40f097e494731ae6f40c
Fabio <90531115+fanaia@users.noreply.github.com>
2024-06-06 16:17:25 -0300

    cotacao-token

--- src/controllers/tokensController.js
+++ src/controllers/tokensController.js
@@ -1,9 +1,10 @@
+const MovimentacaoFinanceira = require("../models/MovimentacaoFinanceira");
 const MovimentacaoToken = require("../models/MovimentacaoToken");
 
 class TokensController {
   static async saldoParticipante(req, res) {
     try {
-      const participanteId = req.user.id;;
+      const participanteId = req.user.id;
       const movimentacoes = await MovimentacaoToken.find({
         participante: participanteId,
         status: "ativo",
@@ -30,6 +31,47 @@ class TokensController {
       });
 
       res.send({ saldo: saldoTotal });
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
+  }
+
+  static async cotacao(req, res) {
+    try {
+      const movimentacaoFinanceira = await MovimentacaoFinanceira.aggregate([
+        {
+          $match: {
+            status: "ativo",
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            saldo: { $sum: { $multiply: ["$valor", "$tipoMovimentacao"] } },
+          },
+        },
+      ]);
+
+      const totalTokens = await MovimentacaoToken.aggregate([
+        {
+          $match: {
+            status: "ativo",
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            total: { $sum: "$valor" },
+          },
+        },
+      ]);
+
+      if (!movimentacaoFinanceira[0] || !totalTokens[0]) {
+        res.send({ cotacao: 0 });
+      } else {
+        const cotacao = movimentacaoFinanceira[0].saldo / totalTokens[0].total;
+        res.send({ cotacao: cotacao });
+      }
     } catch (err) {
       res.status(400).send(err.message);
     }
