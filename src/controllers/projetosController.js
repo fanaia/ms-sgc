055a115954f91911ad1c5313eefa34e3f8ce8dcc
Fabio <90531115+fanaia@users.noreply.github.com>
2024-06-07 16:33:50 -0300

    aprovacao-projetos

--- src/controllers/projetosController.js
+++ src/controllers/projetosController.js
@@ -1,16 +1,32 @@
+const GrupoTrabalho = require("../models/GrupoTrabalho");
 const Projeto = require("../models/Projeto");
 
 class projetosController {
-  static async create(req, res) {
+  static async save(req, res) {
     try {
       const updateData = req.body;
       if (!updateData.nome || updateData.nome == "")
-        return res.status(400).send("Nome is required");
+        return res.status(400).send("Nome é obrigatório");
 
       updateData.participanteInclusao = req.user.id;
       updateData.dataInclusao = new Date();
+      updateData.status = "pendente";
 
-      const projeto = new Projeto(updateData);
+      let projeto;
+      if (req.params.id) {
+        updateData.participanteUltimaAlteracao = req.user.id;
+        updateData.dataUltimaAlteracao = new Date();
+
+        projeto = await Projeto.findByIdAndUpdate(req.params.id, updateData, {
+          new: true,
+        });
+        if (!projeto) {
+          return res.status(404).send("No projeto found with given id");
+        }
+      } else {
+        projeto = new Projeto(updateData);
+      }
+
       await projeto.save();
       res.status(201).send(projeto);
     } catch (err) {
@@ -63,6 +79,34 @@ class projetosController {
     participante.dataUltimaAlteracao = new Date();
 
     res.status(204).send();
+  }
+
+  static async approve(req, res) {
+    try {
+      const responsavel = await GrupoTrabalho.findOne({ participanteResponsavel: req.user.id });
+      if (!responsavel) {
+        return res.status(403).send("Somente responsáveis por GT podem fazer aprovação");
+      }
+
+      const projeto = await Projeto.findById(req.params.id);
+      if (!projeto) {
+        return res.status(404).send("Projeto não encontrado");
+      }
+
+      if (req.user.id == projeto.participanteUltimaAlteracao._id && req.params.approve === "true") {
+        return res.status(403).send("Não é possível aprovar a própria alteração");
+      }
+
+      projeto.status = req.params.approve === "true" ? "ativo" : "recusado";
+
+      projeto.participanteUltimaAlteracao = req.user.id;
+      projeto.dataUltimaAlteracao = new Date();
+
+      await projeto.save();
+      res.status(200).send(projeto);
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
   }
 }
 
--- src/routers/projetosRouter.js
+++ src/routers/projetosRouter.js
@@ -2,10 +2,11 @@ const express = require("express");
 const projetosController = require("../controllers/projetosController");
 
 const router = express.Router();
-router.post("/", projetosController.create);
+router.post("/", projetosController.save);
 router.get("/", projetosController.readAll);
 router.get("/:id", projetosController.readOne);
-router.patch("/:id", projetosController.update);
+router.patch("/:id", projetosController.save);
 router.delete("/:id", projetosController.delete);
+router.put("/:id/approve/:approve", projetosController.approve);
 
 module.exports = router;
