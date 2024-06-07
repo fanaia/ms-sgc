f0e4d2305dab7d6a46316495c2c72972ca4582f8
Fabio <90531115+fanaia@users.noreply.github.com>
2024-06-07 15:58:39 -0300

    aprovacao-grupoTrabalho

--- src/controllers/gruposTrabalhoController.js
+++ src/controllers/gruposTrabalhoController.js
@@ -1,16 +1,31 @@
 const GrupoTrabalho = require("../models/GrupoTrabalho");
 
 class grupoTrabalhosController {
-  static async create(req, res) {
+  static async save(req, res) {
     try {
       const updateData = req.body;
       if (!updateData.nome || updateData.nome == "")
-        return res.status(400).send("Nome is required");
+        return res.status(400).send("Nome é obrigatório");
 
       updateData.participanteInclusao = req.user.id;
       updateData.dataInclusao = new Date();
+      updateData.status = "pendente";
 
-      const grupoTrabalho = new GrupoTrabalho(updateData);
+      let grupoTrabalho;
+      if (req.params.id) {
+        updateData.participanteUltimaAlteracao = req.user.id;
+        updateData.dataUltimaAlteracao = new Date();
+
+        grupoTrabalho = await GrupoTrabalho.findByIdAndUpdate(req.params.id, updateData, {
+          new: true,
+        });
+        if (!grupoTrabalho) {
+          return res.status(404).send("No grupoTrabalho found with given id");
+        }
+      } else {
+        grupoTrabalho = new GrupoTrabalho(updateData);
+      }
+
       await grupoTrabalho.save();
       res.status(201).send(grupoTrabalho);
     } catch (err) {
@@ -33,26 +48,6 @@ class grupoTrabalhosController {
     res.send(grupoTrabalho);
   }
 
-  static async update(req, res) {
-    try {
-      const updateData = req.body;
-      if (updateData.nome == "") return res.status(400).send("Nome is required");
-
-      updateData.participanteUltimaAlteracao = req.user.id;
-      updateData.dataUltimaAlteracao = new Date();
-
-      const grupoTrabalho = await GrupoTrabalho.findByIdAndUpdate(req.params.id, updateData, {
-        new: true,
-      });
-      if (!grupoTrabalho) {
-        return res.status(404).send("Grupo de Trabalho not found");
-      }
-      res.send(grupoTrabalho);
-    } catch (err) {
-      res.status(400).send(err.message);
-    }
-  }
-
   static async delete(req, res) {
     const grupoTrabalho = await GrupoTrabalho.findById(req.params.id);
     if (!grupoTrabalho) {
@@ -66,6 +61,37 @@ class grupoTrabalhosController {
     await grupoTrabalho.save();
 
     res.status(204).send();
+  }
+
+  static async approve(req, res) {
+    try {
+      const responsavel = await GrupoTrabalho.findOne({ participanteResponsavel: req.user.id });
+      if (!responsavel) {
+        return res.status(403).send("Somente responsáveis por GT podem fazer aprovação");
+      }
+
+      const grupoTrabalho = await GrupoTrabalho.findById(req.params.id);
+      if (!grupoTrabalho) {
+        return res.status(404).send("GT não encontrado");
+      }
+
+      if (
+        req.user.id == grupoTrabalho.participanteUltimaAlteracao._id &&
+        req.params.approve === "true"
+      ) {
+        return res.status(403).send("Não é possível aprovar a própria alteração");
+      }
+
+      grupoTrabalho.status = req.params.approve === "true" ? "ativo" : "recusado";
+
+      grupoTrabalho.participanteUltimaAlteracao = req.user.id;
+      grupoTrabalho.dataUltimaAlteracao = new Date();
+
+      await grupoTrabalho.save();
+      res.status(200).send(grupoTrabalho);
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
   }
 }
 
